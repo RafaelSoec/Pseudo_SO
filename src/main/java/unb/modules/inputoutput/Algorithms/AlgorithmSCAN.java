67c048763a4c3bbe76cea083c86496df76e9ae78
jrodriguesmagal <jrodriguesmagal@gmail.com>
2021-05-13 17:54:39 -0300

    Algorithm SCAN fixed

--- src/main/java/unb/modules/inputoutput/Algorithms/AlgorithmSCAN.java
+++ src/main/java/unb/modules/inputoutput/Algorithms/AlgorithmSCAN.java
@@ -13,9 +13,15 @@ public class AlgorithmSCAN {
         ResultInputOutputAlgorithm result = new ResultInputOutputAlgorithm();
         result.setAlgorithmName("SCAN");
         List<Integer> listRequests = new ArrayList<>(diskDriver.getRequests());
+        List<Integer> listAux = new ArrayList<>();
+        Integer totalCylinders = 0, newCurrent = 0, oldValue = diskDriver.getcurrentCylinder();
+        int test = diskDriver.getLastCylinder() + 1, aux =0;
 
-        Integer totalCylinders = 0, newCurrent = 0, oldValue = 0;
-        int test = diskDriver.getLastCylinder() + 1;
+        for(Integer element : listRequests){
+            if (element < oldValue){
+                listAux.add(element);
+            }
+        }
         for (int i = 0; i < listRequests.size() - 1 ; i++){
 
             if (listRequests.get(i) < diskDriver.getcurrentCylinder() && test > diskDriver.getcurrentCylinder() - listRequests.get(i)) {
@@ -27,23 +33,28 @@ public class AlgorithmSCAN {
         }
 
         oldValue = listRequests.get(newCurrent);
-
+        int originalSize = listRequests.size();
         listRequests.remove(listRequests.get(newCurrent));
 
         totalCylinders += test;
 
+
         while (listRequests.size() != 0) {
             test = diskDriver.getLastCylinder() + 1;
-            for (int i = 0; i < listRequests.size() ; i++){
+            while (listRequests.size() + listAux.size() != originalSize) {
+                for (int i = 0; i < listRequests.size(); i++) {
 
-                if (listRequests.get(i) < oldValue && test > oldValue - listRequests.get(i)) {
-                    test = oldValue - listRequests.get(i);
-                    newCurrent = i;
-                    totalCylinders += test;
-                    oldValue = listRequests.get(newCurrent);
-                    listRequests.remove(listRequests.get(newCurrent));
+                    if (listRequests.get(i) < oldValue && test > oldValue - listRequests.get(i)) {
+                        test = oldValue - listRequests.get(i);
+                        newCurrent = i;
+
+                    }
+
                 }
-
+                totalCylinders += test;
+                test = diskDriver.getLastCylinder() + 1;
+                oldValue = listRequests.get(newCurrent);
+                listRequests.remove(listRequests.get(newCurrent));
             }
             totalCylinders += oldValue;
             oldValue = 0;
